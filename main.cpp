760ca1c0c83369e12bd7bf794ca6a178fcdfd392
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2022-02-15 08:27:31 -0500

    Optimized reading file and fixed wide char bug

--- main.cpp
+++ main.cpp
@@ -7,47 +7,44 @@
 #include <string>
 
 int main() {
+	//Set Console mode
 	_setmode(_fileno(stdout), _O_U8TEXT);
 	
-	std::string input;
+	std::wstring input;
 
-	std::string str;
-	std::stringstream ss;
-
-
-
-	str = "â à ä ç é è ê ë î ï ô ù û ü œ";
-	ss << str << "and" << str;
+	std::wstring str;
+	std::wstringstream ss;
 
 
+	//Testing for a string and stringstream
+	str = L"â à ä ç é è ê ë î ï ô ù û ü";
+	ss << str << L" and " << str;
+
+
+	//Write to File
+	std::wofstream outFile("accentedChar.txt");
+	if (outFile.is_open()) {
+
+		outFile << "Writing string: " << str << std::endl;
+		outFile << "Writing stringstream: " << ss.rdbuf() << std::endl;
+	}
+	else {
+		std::wcout << L"Could not open file";
+	}
 	
-		std::ofstream outFile("accentedChar.txt");
-
-		if (outFile.is_open()) {
-
-			outFile << "Writing string: " << str << std::endl;
-			outFile << "Writing stringstream: " << ss.rdbuf() << std::endl;
-		}
-		else {
-			std::wcout << "Could not open file";
-		}
-	
-
-	
-		std::ifstream inFile("accentedChar.txt");
-
-		if (inFile.is_open()) {
-
-			while (getline(inFile, input)) {
-
-				std::cout << input << std::endl;
-			}
-		}
-		else {
-			std::cout << "Could not open file";
-		}
-	
-
+	//Read from file to validate integrity
+	std::wifstream ResultReader("accentedChar.txt", std::ios::binary | std::ios::ate);
+	if (ResultReader.is_open()) {
+		auto FileSize = ResultReader.tellg();
+		ResultReader.seekg(std::ios::beg);
+		std::wstring Results(FileSize, 0);
+		ResultReader.read(&Results[0], FileSize);
+		std::wcout << Results;
+	}
+	else {
+		std::wcout << L"Could not find file." << std::endl;
+	}
+		
 
 
 
